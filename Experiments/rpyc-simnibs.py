a69f533bcd4af7e15f7f607e7ababf1945580a59
Steve Pieper <pieper@isomics.com>
2025-06-06 10:11:01 -0400

    Some notes

--- Experiments/rpyc-simnibs.py
+++ Experiments/rpyc-simnibs.py
@@ -1,5 +1,9 @@
+"""
+/Users/pieper/Applications/SimNIBS-4.5/bin/simnibs_python -m pip install rpyc
+"""
 
 simnibs_python_path = "/Users/pieper/Applications/SimNIBS-4.5/bin/simnibs_python"
+rpyc_path = /Users/pieper/Applications/SimNIBS-4.5/simnibs_env/bin/rpyc_classic
 
 import subprocess
 
