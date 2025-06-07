aae6f953355a315ca506b8d4ff57f6da72fd36d3
Steve Pieper <pieper@isomics.com>
2025-06-07 18:34:53 -0400

    Fix creation of Unstructured Grid Model from SimNIBS mesh object

--- Experiments/rpyc-simnibs.py
+++ Experiments/rpyc-simnibs.py
@@ -6,12 +6,20 @@ simnibs_python_path = "/Users/pieper/Applications/SimNIBS-4.5/bin/simnibs_python
 
 rpyc_path = "/Users/pieper/Applications/SimNIBS-4.5/simnibs_env/bin/rpyc_classic"
 
+try:
+    process
+except NameError:
+    pass
+else:
+    process.kill()
+
 process = slicer.util.launchConsoleProcess([rpyc_path], useStartupEnvironment=True)
 
 line = process.stdout.readline().strip()
 port = int(line.split(":")[-1])
 
 
+import numpy
 import rpyc
 
 simnibs = rpyc.classic.connect("localhost", port)
@@ -23,12 +31,24 @@ print("Simulation setup complete")
 slicer.app.processEvents()
 
 m = simnibs.eval("m")
-nodeCoords = m.nodes.node_coord
-elementIndices = m.elm.node_number_list
+nodeCoords = numpy.array(m.nodes.node_coord)
+elementIndices = numpy.array(m.elm.node_number_list) - 1
 
 meshGrid = vtk.vtkUnstructuredGrid()
 
+gridPoints = vtk.vtkPoints()
+gridPoints.SetNumberOfPoints(len(nodeCoords))
+gridPointsArray = vtk.util.numpy_support.vtk_to_numpy(gridPoints.GetData())
+gridPointsArray[:] = nodeCoords
+meshGrid.SetPoints(gridPoints)
 
+offsetsArray = numpy.arange(0, countsArray.shape[0] * 4, 4, dtype=numpy.int64)
+gridCellVTKArray = vtk.vtkCellArray()
+offsetsVTKArray = vtk.util.numpy_support.numpy_to_vtk(offsetsArray, deep=True)
+tetraIndexVTKArray = vtk.util.numpy_support.numpy_to_vtk(elementIndices.ravel(), deep=True)
+gridCellVTKArray.SetData(offsetsVTKArray, tetraIndexVTKArray)
+meshGrid.SetCells(vtk.VTK_TETRA, gridCellVTKArray)
 
 meshNode = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLModelNode")
+meshNode.SetAndObserveMesh(meshGrid)
 meshNode.CreateDefaultDisplayNodes()
